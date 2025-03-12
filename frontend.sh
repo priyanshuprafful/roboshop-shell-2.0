2f3b5dd4d930b8dbbb4e3d2319ee6ad6a6d1ab35
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-13 01:51:46 +0530

    updating ./roboshop-shell-2.0

--- frontend.sh
+++ frontend.sh
@@ -5,11 +5,11 @@ echo -e "\e[31mRemoving Old Content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[31mDownloading Frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 2>/tmp/roboshop.log
 
 echo -e "\e[31mExtracting Frontend Content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip 2>/tmp/roboshop.log
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 ## we need to copy config file
 echo -e "\e[31mStarting Nginx Server\e[0m"

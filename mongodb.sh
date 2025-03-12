2627f1162a919f7cf46a951dab50153f13a117c6
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-13 02:11:08 +0530

    updating ./roboshop-shell-2.0

---
+++ mongodb.sh
@@ -0,0 +1,9 @@
+echo -e "\e[31mCopy MongoDB repo file \e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[31mInstalling MongoDB Server\e[0m"
+dnf install mongodb-org -y
+
+echo -e "\e[31mStart MongoDB service\e[0m"
+systemctl enable mongod
+systemctl restart mongod

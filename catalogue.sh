c0910546a246714ccccf3044b193fc90a523c92a
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-14 02:29:01 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,8 @@
 component=catalogue
 color="\e[33m"
 nocolor="\e[0m" # It disables the color
-log_file="/tmp/roboshop.log"
+log_file="/tmp/roboshop.log" # here if special character are there then it will ignore the double quote 
+app_path="/app"
 
 echo -e "${color}Disable NodeJS module ${nocolor}"
 dnf module disable nodejs -y &>>${log_file}
@@ -19,16 +20,16 @@ useradd roboshop &>>${log_file}
 
 
 echo -e "${color}Creating App Directory ${nocolor}"
-rm -rf /app # to delete the old content , if present
-mkdir /app
+rm -rf ${app_path} # to delete the old content , if present
+mkdir ${app_path}
 
 echo -e "${color}Download and Extract ${component} Content or application content ${nocolor}"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-cd /app
+cd ${app_path}
 unzip /tmp/${component}.zip &>>${log_file}
 
 echo -e "${color}Installing Dependencies ${nocolor}"
-cd /app
+cd ${app_path}
 npm install &>>${log_file}
 
 echo -e "${color}Copying Updated ${component} service File / Setup SystemD service ${nocolor}"
@@ -56,4 +57,4 @@ dnf install mongodb-org-shell -y &>>${log_file}
 
 
 echo -e "${color}Load Schema ${nocolor}"
-mongo --host mongodb-dev.saraldevops.site </app/schema/${component}.js &>>${log_file}
+mongo --host mongodb-dev.saraldevops.site <${app_path}/schema/${component}.js &>>${log_file}

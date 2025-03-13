f9444e793f6156461b16679c4c711adf2dec4d24
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-13 19:06:05 +0530

    updating ./roboshop-shell-2.0

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+echo -e "\e[31mDisable Mysql module \e[0m"
+dnf module disable mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[31mCopy Repo File \e[0m"
+cp /root/roboshop-shell-2.0/mysql.repo /etc/systemd/system/mysql.repo &>>/tmp/roboshop.log
+
+echo -e "\e[31mInstall Mysql server \e[0m"
+dnf install mysql-community-server -y &>>/tmp/roboshop.log
+
+
+echo -e "\e[31mStarting MySql \e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
+
+
+echo -e "\e[31mSetup and start Mysql server  \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+

6974a97b00a6afa13d103cc6068bba925be078b6
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-13 16:21:10 +0530

    updating ./roboshop-shell-2.0

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[31mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
-echo -e "\e[31mModifying the Config File   \e[0m"
+echo -e "\e[31mModifying the Config File or update the listen address   \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[31mStart MongoDB service\e[0m"

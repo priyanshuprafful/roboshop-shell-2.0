d14a19b5ce473d28a8086ffa450fdbd985daef11
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-14 02:32:33 +0530

    updating ./roboshop-shell-2.0

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-color="\e[33m"
+color="\e[32m"
 nocolor="\e[0m" # It disables the color
 log_file="/tmp/roboshop.log" # here if special character are there then it will ignore the double quote
 app_path="/app"

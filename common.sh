5448c90a1f171aadca0e8a0e8dac16698a232460
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-14 15:31:02 +0530

    updating ./roboshop-shell-2.0

--- common.sh
+++ common.sh
@@ -2,6 +2,37 @@ color="\e[32m"
 nocolor="\e[0m" # It disables the color
 log_file="/tmp/roboshop.log" # here if special character are there then it will ignore the double quote
 app_path="/app"
+app_presetup(){
+    echo -e "${color}Adding Roboshop User or application user ${nocolor}"
+    useradd roboshop &>>${log_file}
+
+    echo -e "${color}Creating App Directory ${nocolor}"
+    rm -rf ${app_path} # to delete the old content , if present
+    mkdir ${app_path}
+
+    echo -e "${color}Download Application content${nocolor}"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+
+
+
+    echo -e "${color}Extracting Application content${nocolor}"
+    cd ${app_path}
+    unzip /tmp/${component}.zip &>>${log_file}
+
+
+}
+
+systemd_setup() {
+
+  echo -e "${color}Setup SystemD service file${nocolor}"
+  cp /root/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  echo -e "${color}Start ${component} Service${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component} &>>${log_file}
+
+
+}
 
 
 nodejs() {
@@ -15,45 +46,57 @@ nodejs() {
   dnf install nodejs -y &>>${log_file}
 
 
-  echo -e "${color}Adding Roboshop User or application user ${nocolor}"
-  useradd roboshop &>>${log_file}
+  app_presetup
 
-
-
-  echo -e "${color}Creating App Directory ${nocolor}"
-  rm -rf ${app_path} # to delete the old content , if present
-  mkdir ${app_path}
-
-  echo -e "${color}Download and Extract ${component} Content or application content ${nocolor}"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  cd ${app_path}
-  unzip /tmp/${component}.zip &>>${log_file}
 
   echo -e "${color}Installing Dependencies ${nocolor}"
   cd ${app_path}
   npm install &>>${log_file}
 
-  echo -e "${color}Copying Updated ${component} service File / Setup SystemD service ${nocolor}"
-  cp /root/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service
-
-  echo -e "${color}Updating the configuration ${nocolor}"
-  systemctl daemon-reload
-
-  echo -e "${color}Start ${component} Service ${nocolor}"
-  systemctl enable ${component} &>>${log_file}
-  systemctl restart ${component} &>>${log_file}
+  systemd_setup
 
 
 }
 
 mongo_schema_setup() {
-  echo -e "\e[31mCopy MongoDB repo file \e[0m"
+  echo -e "${color}Copy MongoDB repo file ${nocolor}"
   cp /root/roboshop-shell-2.0/mongodb.repo /etc/yum.repos.d/mongo.repo
 
-  echo -e "\e[31mInstalling MongoDB server \e[0m"
-  dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+  echo -e "${color}Installing MongoDB server ${nocolor}"
+  dnf install mongodb-org-shell -y &>>${log_file}
 
 
-  echo -e "\e[31mLoad Schema \e[0m"
-  mongo --host mongodb-dev.saraldevops.site </app/schema/user.js &>>/tmp/roboshop.log
+  echo -e "${color}Load Schema ${nocolor}"
+  mongo --host mongodb-dev.saraldevops.site <${app_path}/schema/user.js &>>${log_file}
+}
+mysql_schema_setup(){
+   echo -e "${color}Install MYSQL Client${nocolor}"
+   cp /root/roboshop-shell-2.0/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
+   dnf install mysql -y &>>${log_file}
+
+   echo -e "${color} Load Schema${nocolor}"
+   mysql -h mysql-dev.saraldevops.site -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql &>>${log_file}
+}
+
+maven(){
+  echo -e "${color}Install Maven ${nocolor}"
+  dnf install maven -y &>>${log_file}
+
+
+  app_presetup
+
+
+  echo -e "${color} Download Application Dependencies${nocolor}"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+
+
+
+
+
+  mysql_schema_setup
+  systemd_setup
+
+
 }

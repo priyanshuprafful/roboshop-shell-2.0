4660712f7066165e0057fd4530f7a6cea49529f6
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-14 13:28:54 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,50 +1,7 @@
 source common.sh
 component=catalogue
 
-echo -e "${color}Disable NodeJS module ${nocolor}"
-dnf module disable nodejs -y &>>${log_file}
-
-echo -e "${color}Enabling NodeJS 18 module ${nocolor}"
-dnf module enable nodejs:18 -y &>>${log_file}
-
-echo -e "${color}Installing NodeJS ${nocolor}"
-dnf install nodejs -y &>>${log_file}
-
-
-echo -e "${color}Adding Roboshop User or application user ${nocolor}"
-useradd roboshop &>>${log_file}
-
-
-
-echo -e "${color}Creating App Directory ${nocolor}"
-rm -rf ${app_path} # to delete the old content , if present
-mkdir ${app_path}
-
-echo -e "${color}Download and Extract ${component} Content or application content ${nocolor}"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-cd ${app_path}
-unzip /tmp/${component}.zip &>>${log_file}
-
-echo -e "${color}Installing Dependencies ${nocolor}"
-cd ${app_path}
-npm install &>>${log_file}
-
-echo -e "${color}Copying Updated ${component} service File / Setup SystemD service ${nocolor}"
-cp /root/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service
-
-echo -e "${color}Updating the configuration ${nocolor}"
-systemctl daemon-reload
-
-echo -e "${color}Start ${component} Service ${nocolor}"
-systemctl enable ${component} &>>${log_file}
-systemctl restart ${component} &>>${log_file}
-
-
-
-
-
-
-
+nodejs
 
 echo -e "${color}Copy MongoDB repo file ${nocolor}"
 cp /root/roboshop-shell-2.0/mongodb.repo /etc/yum.repos.d/mongo.repo
--- common.sh
+++ common.sh
@@ -2,3 +2,46 @@ color="\e[32m"
 nocolor="\e[0m" # It disables the color
 log_file="/tmp/roboshop.log" # here if special character are there then it will ignore the double quote
 app_path="/app"
+
+
+nodejs() {
+  echo -e "${color}Disable NodeJS module ${nocolor}"
+  dnf module disable nodejs -y &>>${log_file}
+
+  echo -e "${color}Enabling NodeJS 18 module ${nocolor}"
+  dnf module enable nodejs:18 -y &>>${log_file}
+
+  echo -e "${color}Installing NodeJS ${nocolor}"
+  dnf install nodejs -y &>>${log_file}
+
+
+  echo -e "${color}Adding Roboshop User or application user ${nocolor}"
+  useradd roboshop &>>${log_file}
+
+
+
+  echo -e "${color}Creating App Directory ${nocolor}"
+  rm -rf ${app_path} # to delete the old content , if present
+  mkdir ${app_path}
+
+  echo -e "${color}Download and Extract ${component} Content or application content ${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  cd ${app_path}
+  unzip /tmp/${component}.zip &>>${log_file}
+
+  echo -e "${color}Installing Dependencies ${nocolor}"
+  cd ${app_path}
+  npm install &>>${log_file}
+
+  echo -e "${color}Copying Updated ${component} service File / Setup SystemD service ${nocolor}"
+  cp /root/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service
+
+  echo -e "${color}Updating the configuration ${nocolor}"
+  systemctl daemon-reload
+
+  echo -e "${color}Start ${component} Service ${nocolor}"
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component} &>>${log_file}
+
+
+}

680e8c7a21d3b4acdfb9ee3be12a58e3fe0fcb39
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-15 19:20:33 +0530

    updating ./roboshop-shell-2.0

--- mongodb.sh
+++ mongodb.sh
@@ -3,18 +3,18 @@ component=mongodb
 
 echo -e "${color}Copy MongoDB repo file ${nocolor}"
 cp /home/centos/roboshop-shell-2.0/mongodb.repo /etc/yum.repos.d/mongo.repo
-stat_check $?
+status_check $?
 
 echo -e "${color}Installing MongoDB Server${nocolor}"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
-stat_check $?
+status_check $?
 
 echo -e "${color}Modifying the Config File or update the listen address   ${nocolor}"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat_check $?
+status_check $?
 
 echo -e "${color}Start MongoDB service${nocolor}"
 systemctl enable mongod
 systemctl restart mongod
-stat_check $?
+status_check $?
 

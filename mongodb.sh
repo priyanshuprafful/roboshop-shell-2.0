d4d2810415a495fc0fbfa90f635bd6731c7db92d
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-07 19:53:02 +0530

    updating ./roboshop-shell-2.0

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,8 @@
-printf "\e[33mCopying MongoDB Repo \e[0m"
-cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[33mCopying MongoDB Repo \e[0m"
+cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 # Need to update listen address
 echo -e "\e[33mStarting MongoDB Server\e[0m"

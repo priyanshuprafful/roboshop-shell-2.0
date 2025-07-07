45b197f6568fd4d2e6b3ccd90af3f9cd670d5b90
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-07 19:37:58 +0530

    updating ./roboshop-shell-2.0

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
 echo -e "\e[33mInstalling Nginx Server\e[0m"
-dnf install nginx -y &>/tmp/roboshop.log
+dnf install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mRemoving old app content\e[0m"
-rm -rf /usr/share/nginx/html/* &>/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mDownloading Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mExtract Frontend Content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 #We need to add config file
 

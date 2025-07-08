b5f866c1a818b149269bbffe07545358a829d7ea
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 18:55:43 +0530

    updating ./roboshop-shell-2.0

--- cart.sh
+++ cart.sh
@@ -1,42 +1,46 @@
-echo -e "\e[33mDisabling nodejs module and enabling 18 module\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+source common_shell_script.sh
 
-echo -e "\e[33mInstalling NodeJs\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+component=cart
 
-echo -e "\e[33mAdding Roboshop cart\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "${color} Disabling nodejs module and enabling 18 module ${exit_color} "
+dnf module disable nodejs -y &>>${log_file} 
+dnf module enable nodejs:18 -y &>>${log_file} 
 
-echo -e "\e[33mCreating App Directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+echo -e "${color} Installing NodeJs ${exit_color} "
+dnf install nodejs -y &>>${log_file} 
 
+echo -e "${color} Adding Roboshop  ${component}  ${exit_color} "
+useradd roboshop &>>${log_file} 
 
-echo -e "\e[33mDownloading and extracting app content \e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
+echo -e "${color} Creating App Directory ${exit_color} "
+rm -rf ${app_path}  &>>${log_file} 
+mkdir ${app_path}  &>>${log_file} 
 
 
-echo -e "\e[33mInstalling Dependencies\e[0m"
-cd /app
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[33mCopying cart Service File\e[0m"
-cp /home/centos/roboshop-shell-2.0/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+echo -e "${color} Downloading and extracting app content  ${exit_color} "
+curl -o /tmp/ ${component} .zip https://roboshop-artifacts.s3.amazonaws.com/ ${component} .zip &>>${log_file} 
+cd ${app_path} 
+unzip /tmp/ ${component} .zip &>>${log_file} 
 
 
-echo -e "\e[33mStarting cart Service\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
+echo -e "${color} Installing Dependencies ${exit_color} "
+cd ${app_path} 
+npm install &>>${log_file} 
 
-#echo -e "\e[33mCopying Mongodb Repo File\e[0m"
-#cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+echo -e "${color} Copying  ${component}  Service File ${exit_color} "
+cp /home/centos/roboshop-shell-2.0/ ${component} .service /etc/systemd/system/ ${component} .service &>>${log_file} 
+
+
+echo -e "${color} Starting  ${component}  Service ${exit_color} "
+systemctl daemon-reload &>>${log_file} 
+systemctl enable  ${component}  &>>${log_file} 
+systemctl restart  ${component}  &>>${log_file} 
+
+#echo -e "${color} Copying Mongodb Repo File ${exit_color} "
+#cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file} 
 #
-#echo -e "\e[33mInstalling mongodb server\e[0m"
-#dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+#echo -e "${color} Installing mongodb server ${exit_color} "
+#dnf install mongodb-org-shell -y &>>${log_file} 
 #
-#echo -e "\e[33mLoading Schema\e[0m"
-#mongo --host mongodb-dev.devopspro.fun </app/schema/cart.js &>>/tmp/roboshop.log
+#echo -e "${color} Loading Schema ${exit_color} "
+#mongo --host mongodb-dev.devopspro.fun <${app_path} /schema/ ${component} .js &>>${log_file} 

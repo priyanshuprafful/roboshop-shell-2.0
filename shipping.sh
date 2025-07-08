674f67756aebc0c3964587ed69bd024212fa289b
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 20:06:51 +0530

    updating ./roboshop-shell-2.0

--- shipping.sh
+++ shipping.sh
@@ -4,11 +4,3 @@ component=shipping
 maven
 
 
-echo -e "\e[33mInstalling Mysql\e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
-
-
-echo -e "\e[33mLoading Schema\e[0m"
-mysql -h mysql-dev.devopspro.fun -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-
-systemd_setup

dfcfd155317cb99e128afc63559c0802ef0985a6
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 19:17:42 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -2,41 +2,8 @@ source common_shell_script.sh
 
 component=catalogue
 
+nodejs
 
-
-echo -e "${color} Disabling nodejs module and enabling 18 module ${exit_color}"
-dnf module disable nodejs -y &>>${log_file}
-dnf module enable nodejs:18 -y &>>${log_file}
-
-echo -e "${color} Installing NodeJs ${exit_color}"
-dnf install nodejs -y &>>${log_file}
-
-echo -e "${color} Adding Roboshop User ${exit_color}"
-useradd roboshop &>>${log_file}
-
-echo -e "${color} Creating App Directory ${exit_color}"
-rm -rf ${app_path} &>>${log_file}
-mkdir ${app_path} &>>${log_file}
-
-
-echo -e "${color} Downloading and extracting app content ${exit_color}"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-cd ${app_path}
-unzip /tmp/${component}.zip &>>${log_file}
-
-
-echo -e "${color} Installing Dependencies ${exit_color}"
-cd ${app_path}
-npm install &>>${log_file}
-
-echo -e "${color} Copying ${component} Service File ${exit_color}"
-cp /home/centos/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-
-
-echo -e "${color} Starting ${component} Service ${exit_color}"
-systemctl daemon-reload &>>${log_file}
-systemctl enable ${component} &>>${log_file}
-systemctl start ${component} &>>${log_file}
 
 echo -e "${color} Copying Mongodb Repo File ${exit_color}"
 cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
--- common_shell_script.sh
+++ common_shell_script.sh
@@ -2,3 +2,41 @@ color="\e[36m"
 exit_color="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+
+
+nodejs() {
+  echo -e "${color} Disabling nodejs module and enabling 18 module ${exit_color}"
+  dnf module disable nodejs -y &>>${log_file}
+  dnf module enable nodejs:18 -y &>>${log_file}
+
+  echo -e "${color} Installing NodeJs ${exit_color}"
+  dnf install nodejs -y &>>${log_file}
+
+  echo -e "${color} Adding Roboshop User ${exit_color}"
+  useradd roboshop &>>${log_file}
+
+  echo -e "${color} Creating App Directory ${exit_color}"
+  rm -rf ${app_path} &>>${log_file}
+  mkdir ${app_path} &>>${log_file}
+
+
+  echo -e "${color} Downloading and extracting app content ${exit_color}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  cd ${app_path}
+  unzip /tmp/${component}.zip &>>${log_file}
+
+
+  echo -e "${color} Installing Dependencies ${exit_color}"
+  cd ${app_path}
+  npm install &>>${log_file}
+
+  echo -e "${color} Copying ${component} Service File ${exit_color}"
+  cp /home/centos/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+
+  echo -e "${color} Starting ${component} Service ${exit_color}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl start ${component} &>>${log_file}
+
+}

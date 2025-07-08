b62bae08f41619820926b6ffee5f9abd2773b96c
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 18:38:32 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,45 +1,47 @@
 component=catalogue
+color="\e[36m"
+exit_color="\e[0m"
 
 
-echo -e "\e[33mDisabling nodejs module and enabling 18 module\e[0m"
+echo -e "${color} Disabling nodejs module and enabling 18 module ${exit_color}"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling NodeJs\e[0m"
+echo -e "${color} Installing NodeJs ${exit_color}"
 dnf install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mAdding Roboshop User\e[0m"
+echo -e "${color} Adding Roboshop User ${exit_color}"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[33mCreating App Directory\e[0m"
+echo -e "${color} Creating App Directory ${exit_color}"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
 
-echo -e "\e[33mDownloading and extracting app content \e[0m"
+echo -e "${color} Downloading and extracting app content ${exit_color}"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
 cd /app
 unzip /tmp/${component}.zip &>>/tmp/roboshop.log
 
 
-echo -e "\e[33mInstalling Dependencies\e[0m"
+echo -e "${color} Installing Dependencies ${exit_color}"
 cd /app
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[33mCopying ${component} Service File\e[0m"
+echo -e "${color} Copying ${component} Service File ${exit_color}"
 cp /home/centos/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
 
 
-echo -e "\e[33mStarting ${component} Service\e[0m"
+echo -e "${color} Starting ${component} Service ${exit_color}"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable ${component} &>>/tmp/roboshop.log
 systemctl start ${component} &>>/tmp/roboshop.log
 
-echo -e "\e[33mCopying Mongodb Repo File\e[0m"
+echo -e "${color} Copying Mongodb Repo File ${exit_color}"
 cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling mongodb server\e[0m"
+echo -e "${color} Installing mongodb server ${exit_color}"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mLoading Schema\e[0m"
+echo -e "${color} Loading Schema ${exit_color}"
 mongo --host mongodb-dev.devopspro.fun </app/schema/${component}.js &>>/tmp/roboshop.log

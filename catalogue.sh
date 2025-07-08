edd93a4f11bada3aa431bf31333681efd0f0414d
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 18:31:11 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,6 @@
+component=catalogue
+
+
 echo -e "\e[33mDisabling nodejs module and enabling 18 module\e[0m"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
@@ -14,23 +17,23 @@ mkdir /app &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mDownloading and extracting app content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mInstalling Dependencies\e[0m"
 cd /app
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[33mCopying Catalogue Service File\e[0m"
-cp /home/centos/roboshop-shell-2.0/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+echo -e "\e[33mCopying ${component} Service File\e[0m"
+cp /home/centos/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
 
 
-echo -e "\e[33mStarting Catalogue Service\e[0m"
+echo -e "\e[33mStarting ${component} Service\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue &>>/tmp/roboshop.log
+systemctl enable ${component} &>>/tmp/roboshop.log
+systemctl start ${component} &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying Mongodb Repo File\e[0m"
 cp /home/centos/roboshop-shell-2.0/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
@@ -39,4 +42,4 @@ echo -e "\e[33mInstalling mongodb server\e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mLoading Schema\e[0m"
-mongo --host mongodb-dev.devopspro.fun </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devopspro.fun </app/schema/${component}.js &>>/tmp/roboshop.log

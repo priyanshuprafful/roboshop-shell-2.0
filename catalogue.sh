83b194ea4cd97b65f663d20d54e10b5255c1d755
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 14:42:19 +0530

    updating ./roboshop-shell-2.0

---
+++ catalogue.sh
@@ -0,0 +1,29 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+
+cd /app
+npm install
+
+systemctl daemon-reload
+
+
+systemctl enable catalogue
+systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org-shell -y
+
+mongo --host mongodb-dev.devopspro.fun </app/schema/catalogue.js

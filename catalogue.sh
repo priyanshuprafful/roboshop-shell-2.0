23a20ed84ed6317bf9b307f0b72bd575ec5bdfb9
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 14:55:49 +0530

    updating ./roboshop-shell-2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,41 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+echo -e "\e[33mDisabling nodejs module and enabling 18 module\e[0m"
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-dnf install nodejs -y
+echo -e "\e[33mInstalling NodeJs\e[0m"
+dnf install nodejs -y &>>/tmp/roboshop.log
 
-useradd roboshop
+echo -e "\e[33mAdding Roboshop User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
 
-mkdir /app
+echo -e "\e[33mCreating App Directory\e[0m"
+mkdir /app &>>/tmp/roboshop.log
 
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[33mDownloading and extracting app content \e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
 
+echo -e "\e[33mInstalling Dependencies\e[0m"
 cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 
-systemctl daemon-reload
+echo -e "\e[33mCopying Catalogue Service File\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 
-systemctl enable catalogue
-systemctl start catalogue
+echo -e "\e[33mStarting Catalogue Service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[33mCopying Mongodb Repo File\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-dnf install mongodb-org-shell -y
+echo -e "\e[3333mInstalling mongodb server\e[0m"
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
+echo -e "\e[33mLoading Schema\e[0m"
 mongo --host mongodb-dev.devopspro.fun </app/schema/catalogue.js

7d243c8d257f93e0abfa1b3a0e8ad4eb306aadb4
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-08 15:17:22 +0530

    updating ./roboshop-shell-2.0

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate MongoDB Listen Address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting MongoDB Server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log

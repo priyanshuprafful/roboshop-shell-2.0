a53dfffe8f8e6fe6ea5bcfaccfa0c42f0661512c
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-13 17:33:41 +0530

    updating ./roboshop-shell-2.0

--- common_shell_script.sh
+++ common_shell_script.sh
@@ -40,8 +40,9 @@ systemd_setup() {
     echo -e "${color} Copying ${component} Service File ${exit_color}"
     cp /home/centos/roboshop-shell-2.0/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
     sed -i -e "s/roboshop_app_password/${roboshop_app_password}/" /etc/systemd/system/${component}.service
-
     exit_status $?
+
+
     echo -e "${color} Starting ${component} Service ${exit_color}"
     systemctl daemon-reload &>>${log_file}
     systemctl enable ${component} &>>${log_file}
